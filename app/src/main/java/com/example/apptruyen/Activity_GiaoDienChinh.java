9a7fbc066942c181d84a247008e3fe6494a1276b
nghia <hothinh00191@gmail.com>
2022-11-12 09:04:11 +0700

    helo

---
+++ app/src/main/java/com/example/apptruyen/Activity_GiaoDienChinh.java
@@ -0,0 +1,14 @@
+package com.example.apptruyen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_GiaoDienChinh extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_giao_dien_chinh);
+    }
+}
---
+++ app/src/main/java/com/example/apptruyen/Activity_PhanLoai.java
@@ -0,0 +1,14 @@
+package com.example.apptruyen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_PhanLoai extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phan_loai);
+    }
+}
